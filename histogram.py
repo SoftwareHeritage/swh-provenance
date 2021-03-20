3933c17f5e0189bee0b09364f016a56da26b2af3
Andres Ezequiel Viso <aeviso@softwareheritage.org>
2021-03-20 15:27:15 +0100

    Fixed histogram calculation to distinguish root.

--- histogram.py
+++ histogram.py
@@ -23,14 +23,21 @@ if __name__ == "__main__":
 
     for table in tables:
         provenance.cursor.execute(f"""SELECT depths.depth, COUNT(depths.depth)
-                                        FROM (SELECT (CHAR_LENGTH(ENCODE(location.path, 'escape')) - CHAR_LENGTH(REPLACE(ENCODE(location.path, 'escape'), '/', ''))) / CHAR_LENGTH('/') AS depth
+                                        FROM (SELECT 
+                                                CASE location.path
+                                                    WHEN '' THEN 0
+                                                    WHEN '.' THEN 0
+                                                    ELSE 1 + CHAR_LENGTH(ENCODE(location.path, 'escape')) - 
+                                                             CHAR_LENGTH(REPLACE(ENCODE(location.path, 'escape'), '/', ''))
+                                                END AS depth
                                                 FROM {table}
                                                 JOIN location
                                                   ON {table}.loc=location.id
                                              ) AS depths
                                         GROUP BY depths.depth
                                         ORDER BY depths.depth""")
-        with io.open(conninfo["db"]["dbname"] + f"_{table}.csv", "w") as outfile:
+        filename = "depths_" + conninfo["db"]["dbname"] + f"_{table}.csv"
+        with io.open(filename, "w") as outfile:
             outfile.write(f"{table} depth,{table} count\n")
             for depth, count in provenance.cursor.fetchall():
                 outfile.write(f"{depth},{count}\n")

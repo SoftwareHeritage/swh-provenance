245d28bd45db81d8876c3418394ea4331f1bd5e1
David Douard <david.douard@sdfa3.org>
2024-05-14 18:05:17 +0200

    Add scaffolding for testing the swh-graph backend

--- swh/provenance/backend/graph.py
+++ swh/provenance/backend/graph.py
@@ -3,12 +3,16 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
+import grpc
+from swh.graph.grpc import swhgraph_pb2_grpc
 from swh.model.swhids import CoreSWHID
 
 
 class GraphProvenance:
-    def __init__(self, graph):
-        self.graph = graph
+    def __init__(self, url):
+        self.graph_url = url
+        self._channel = grpc.insecure_channel(self.graph_url)
+        self._stub = swhgraph_pb2_grpc.TraversalServiceStub(self._channel)
 
     def check_config(self) -> bool:
         return True
